5fc6b64e1fb6a3c37bf9689ed49f083d93aa5467
xenxi <antoniom.diaz.moreno@gmail.com>
2021-10-04 07:18:47 +0200

    add animations

--- lib/presentation/coming_soon_page.dart
+++ lib/presentation/coming_soon_page.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:cv_desing_website_flutter/presentation/shared/logo_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -36,10 +37,13 @@ class ComingSoonPage extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(top: 50),
       child: Center(
-        child: LogoWidget(
-          height: logoHeight,
-          minHeight: 250,
-          // color: Colors.amberAccent,
+        child: FlipInY(
+          delay: const Duration(milliseconds: 700),
+          child: LogoWidget(
+            height: logoHeight,
+            minHeight: 250,
+            // color: Colors.amberAccent,
+          ),
         ),
       ),
     );
@@ -47,20 +51,25 @@ class ComingSoonPage extends StatelessWidget {
 
   List<Widget> _buildText(BuildContext context) => [
         Center(
-            child: Text(
-          'Oops...',
-          style: Theme.of(context)
-              .textTheme
-              .headline1!
-              .copyWith(color: const Color.fromRGBO(194, 162, 126, 1)),
+            child: FadeInLeft(
+          child: Text(
+            'Oops...',
+            style: Theme.of(context)
+                .textTheme
+                .headline1!
+                .copyWith(color: const Color.fromRGBO(194, 162, 126, 1)),
+          ),
         )),
         Center(
-            child: Text(
-          'En construcción',
-          style: Theme.of(context)
-              .textTheme
-              .headline5!
-              .copyWith(color: const Color.fromRGBO(194, 162, 126, 1)),
+            child: FadeInRight(
+          delay: const Duration(milliseconds: 500),
+          child: Text(
+            'En construcción',
+            style: Theme.of(context)
+                .textTheme
+                .headline5!
+                .copyWith(color: const Color.fromRGBO(194, 162, 126, 1)),
+          ),
         )),
       ];
 
@@ -68,18 +77,27 @@ class ComingSoonPage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          _buildSocialLinkButton(context,
-              icon: FontAwesomeIcons.envelope,
-              onPressed: () async => await _launchURL(
-                  'mailto:dscurriculumvitae@gmail.com?subject=Información web')),
-          _buildSocialLinkButton(context,
-              icon: FontAwesomeIcons.instagram,
-              onPressed: () async => await _launchURL(
-                  'https://www.instagram.com/dscurriculumvitae')),
-          _buildSocialLinkButton(context,
-              icon: FontAwesomeIcons.tiktok,
-              onPressed: () async => await _launchURL(
-                  'https://www.tiktok.com/@dscurriculumvitae')),
+          ElasticIn(
+            delay: const Duration(milliseconds: 1000),
+            child: _buildSocialLinkButton(context,
+                icon: FontAwesomeIcons.envelope,
+                onPressed: () async => await _launchURL(
+                    'mailto:dscurriculumvitae@gmail.com?subject=Información web')),
+          ),
+          ElasticIn(
+            delay: const Duration(milliseconds: 1500),
+            child: _buildSocialLinkButton(context,
+                icon: FontAwesomeIcons.instagram,
+                onPressed: () async => await _launchURL(
+                    'https://www.instagram.com/dscurriculumvitae')),
+          ),
+          ElasticIn(
+            delay: const Duration(milliseconds: 2000),
+            child: _buildSocialLinkButton(context,
+                icon: FontAwesomeIcons.tiktok,
+                onPressed: () async => await _launchURL(
+                    'https://www.tiktok.com/@dscurriculumvitae')),
+          ),
         ],
       );
 
