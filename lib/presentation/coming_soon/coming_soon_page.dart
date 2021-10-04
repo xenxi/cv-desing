9ed7c9e08fc997f4e784c0453e470ac9fbda4c02
xenxi <antoniom.diaz.moreno@gmail.com>
2021-10-04 07:28:20 +0200

    extract social widget

--- lib/presentation/coming_soon/coming_soon_page.dart
+++ lib/presentation/coming_soon/coming_soon_page.dart
@@ -1,8 +1,7 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:cv_desing_website_flutter/presentation/shared/logo_widget.dart';
+import 'package:cv_desing_website_flutter/presentation/shared/social_links_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class ComingSoonPage extends StatelessWidget {
   const ComingSoonPage({Key? key}) : super(key: key);
@@ -25,7 +24,7 @@ class ComingSoonPage extends StatelessWidget {
               const SizedBox(
                 height: 20,
               ),
-              _buildSocialLinks(context),
+              const SocialLinksWidget(),
             ],
           ),
         ),
@@ -72,44 +71,4 @@ class ComingSoonPage extends StatelessWidget {
           ),
         )),
       ];
-
-  Widget _buildSocialLinks(BuildContext context) => Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElasticIn(
-            delay: const Duration(milliseconds: 1000),
-            child: _buildSocialLinkButton(context,
-                icon: FontAwesomeIcons.envelope,
-                onPressed: () async => await _launchURL(
-                    'mailto:dscurriculumvitae@gmail.com?subject=Información web')),
-          ),
-          ElasticIn(
-            delay: const Duration(milliseconds: 1500),
-            child: _buildSocialLinkButton(context,
-                icon: FontAwesomeIcons.instagram,
-                onPressed: () async => await _launchURL(
-                    'https://www.instagram.com/dscurriculumvitae')),
-          ),
-          ElasticIn(
-            delay: const Duration(milliseconds: 2000),
-            child: _buildSocialLinkButton(context,
-                icon: FontAwesomeIcons.tiktok,
-                onPressed: () async => await _launchURL(
-                    'https://www.tiktok.com/@dscurriculumvitae')),
-          ),
-        ],
-      );
-
-  Future<void> _launchURL(String url) async {
-    await canLaunch(url) ? await launch(url) : throw 'Could not launch $url';
-  }
-
-  Widget _buildSocialLinkButton(BuildContext context,
-          {required IconData icon, required void Function() onPressed}) =>
-      IconButton(
-        onPressed: onPressed,
-        icon: FaIcon(icon),
-        color: const Color.fromRGBO(194, 162, 126, 1),
-      );
 }
---
+++ lib/presentation/shared/social_links_widget.dart
@@ -0,0 +1,51 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class SocialLinksWidget extends StatelessWidget {
+  const SocialLinksWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElasticIn(
+          delay: const Duration(milliseconds: 1000),
+          child: _buildSocialLinkButton(context,
+              icon: FontAwesomeIcons.envelope,
+              onPressed: () async => await _launchURL(
+                  'mailto:dscurriculumvitae@gmail.com?subject=Información web')),
+        ),
+        ElasticIn(
+          delay: const Duration(milliseconds: 1500),
+          child: _buildSocialLinkButton(context,
+              icon: FontAwesomeIcons.instagram,
+              onPressed: () async => await _launchURL(
+                  'https://www.instagram.com/dscurriculumvitae')),
+        ),
+        ElasticIn(
+          delay: const Duration(milliseconds: 2000),
+          child: _buildSocialLinkButton(context,
+              icon: FontAwesomeIcons.tiktok,
+              onPressed: () async => await _launchURL(
+                  'https://www.tiktok.com/@dscurriculumvitae')),
+        ),
+      ],
+    );
+  }
+
+  Future<void> _launchURL(String url) async {
+    await canLaunch(url) ? await launch(url) : throw 'Could not launch $url';
+  }
+
+  Widget _buildSocialLinkButton(BuildContext context,
+          {required IconData icon, required void Function() onPressed}) =>
+      IconButton(
+        onPressed: onPressed,
+        icon: FaIcon(icon),
+        color: const Color.fromRGBO(194, 162, 126, 1),
+      );
+}
