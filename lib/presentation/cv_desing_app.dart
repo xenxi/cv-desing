3fa54552c9823f113e2edf3241b7e326721f21c7
xenxi <antoniom.diaz.moreno@gmail.com>
2021-10-10 11:03:57 +0200

    add home page

--- lib/presentation/cv_desing_app.dart
+++ lib/presentation/cv_desing_app.dart
@@ -3,6 +3,7 @@ import 'package:cv_desing_website_flutter/presentation/core/theme.dart';
 import 'package:flutter/material.dart';
 
 import 'coming_soon/coming_soon_page.dart';
+import 'home/home_page.dart';
 
 class CvDesingApp extends StatelessWidget {
   const CvDesingApp({Key? key}) : super(key: key);
@@ -11,7 +12,7 @@ class CvDesingApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Diseños CV',
-      home: const ComingSoonPage(),
+      home: const HomePage(),
       theme: CustomTheme.lightTheme,
     );
   }
