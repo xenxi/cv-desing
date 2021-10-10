797ecd075002a1c16b5b6e026fc9cc2b4af8f8c9
xenxi <antoniom.diaz.moreno@gmail.com>
2021-10-10 10:52:14 +0200

    remove unused import

--- lib/presentation/portfolio/widgets/project_item/project_item.dart
+++ lib/presentation/portfolio/widgets/project_item/project_item.dart
@@ -1,4 +1,3 @@
-import 'package:animate_do/animate_do.dart';
 import 'package:cv_desing_website_flutter/presentation/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
