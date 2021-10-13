4b167f6e16b52374f4f82beb43036d9370d80f81
xenxi <antoniom.diaz.moreno@gmail.com>
2021-10-13 07:37:36 +0200

    add image thumbnail

--- lib/presentation/home/sections/portfolio/portfolio_section.dart
+++ lib/presentation/home/sections/portfolio/portfolio_section.dart
@@ -1,5 +1,4 @@
 import 'package:cv_desing_website_flutter/domain/curriculum.dart';
-import 'package:cv_desing_website_flutter/presentation/core/adaptative.dart';
 import 'package:cv_desing_website_flutter/presentation/core/theme.dart';
 import 'package:cv_desing_website_flutter/presentation/home/widgets/section.dart';
 import 'package:cv_desing_website_flutter/presentation/shared/section_tittle.dart';
@@ -10,19 +9,19 @@ import 'widgets/project_categories/project_category_data.dart';
 import 'widgets/project_item/project_item.dart';
 
 final curriculumsData = [
-  Curriculum(1, 'CV12A', 'assets/cvs/CV12A.jpg', 'Curriculum'),
-  Curriculum(2, 'CV12B', 'assets/cvs/CV12B.jpg', 'Curriculum'),
-  Curriculum(3, 'CV12C', 'assets/cvs/CV12C.jpg', 'Curriculum'),
-  Curriculum(4, 'CV12E', 'assets/cvs/CV12E.jpg', 'Curriculum'),
-  // Curriculum(5, 'CV12H', 'assets/cvs/CV12H.jpg', 'Curriculum'),
-  // Curriculum(6, 'CV12J', 'assets/cvs/CV12J.jpg', 'Curriculum'),
-  // Curriculum(7, 'CV12K', 'assets/cvs/CV12K.jpg', 'Curriculum'),
-  // Curriculum(8, 'CV12M', 'assets/cvs/CV12M.jpg', 'Curriculum'),
-  // Curriculum(9, 'CV12N', 'assets/cvs/CV12N.jpg', 'Curriculum'),
-  // Curriculum(10, 'CV12O', 'assets/cvs/CV12O.jpg', 'Curriculum'),
-  // Curriculum(11, 'CV12P', 'assets/cvs/CV12P.jpg', 'Curriculum'),
-  // Curriculum(12, 'CV12Q', 'assets/cvs/CV12Q.jpg', 'Curriculum'),
-  // Curriculum(13, 'CV12R', 'assets/cvs/CV12R.jpg', 'Curriculum'),
+  Curriculum(1, 'CV12A', 'assets/cvs/thumbnail/CV12A.jpg', 'Curriculum'),
+  Curriculum(2, 'CV12B', 'assets/cvs/thumbnail/CV12B.jpg', 'Curriculum'),
+  Curriculum(3, 'CV12C', 'assets/cvs/thumbnail/CV12C.jpg', 'Curriculum'),
+  Curriculum(4, 'CV12E', 'assets/cvs/thumbnail/CV12E.jpg', 'Curriculum'),
+  Curriculum(5, 'CV12H', 'assets/cvs/thumbnail/CV12H.jpg', 'Curriculum'),
+  Curriculum(6, 'CV12J', 'assets/cvs/thumbnail/CV12J.jpg', 'Curriculum'),
+  Curriculum(7, 'CV12K', 'assets/cvs/thumbnail/CV12K.jpg', 'Curriculum'),
+  Curriculum(8, 'CV12M', 'assets/cvs/thumbnail/CV12M.jpg', 'Curriculum'),
+  Curriculum(9, 'CV12N', 'assets/cvs/thumbnail/CV12N.jpg', 'Curriculum'),
+  Curriculum(10, 'CV12O', 'assets/cvs/thumbnail/CV12O.jpg', 'Curriculum'),
+  Curriculum(11, 'CV12P', 'assets/cvs/thumbnail/CV12P.jpg', 'Curriculum'),
+  Curriculum(12, 'CV12Q', 'assets/cvs/thumbnail/CV12Q.jpg', 'Curriculum'),
+  Curriculum(13, 'CV12R', 'assets/cvs/thumbnail/CV12R.jpg', 'Curriculum'),
 ];
 final categoriesData = [
   ProjectCategoryData(number: 0, title: 'Test 1'),
@@ -75,17 +74,4 @@ class PortfolioSection extends StatelessWidget {
       ),
     );
   }
-
-  List<Widget> _buildProjects(BuildContext context,
-          {required List<Curriculum> data}) =>
-      List.generate(
-          data.length,
-          (index) => ProjectItem(
-                // width: 300,
-                // height: assignHeight(context: context, fraction: 0.3),
-                // bannerHeight: assignHeight(context: context, fraction: 0.4) / 3,
-                title: data[index].reference,
-                subtitle: data[index].category,
-                imageUrl: data[index].url,
-              ));
 }
