2fdc2987b29681b0299dc70e4184d9a00482a461
xenxi <antoniom.diaz.moreno@gmail.com>
2021-10-27 08:19:03 +0200

    update theme

--- lib/presentation/core/custom_theme.dart
+++ lib/presentation/core/custom_theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CustomTheme {
   static double navbarHeight = 100.0;
@@ -13,7 +14,17 @@ class CustomTheme {
   static ThemeData get lightTheme {
     return ThemeData.light().copyWith(
         primaryColor: primaryColor,
+        textTheme: _lightTextTheme,
         colorScheme: const ColorScheme.light()
             .copyWith(primary: primaryColor, secondary: secondaryColor));
   }
+
+  static final TextTheme _lightTextTheme = ThemeData.light().textTheme.copyWith(
+        caption: GoogleFonts.ibmPlexMono(
+          fontSize: 12,
+          color: primaryText1,
+          fontWeight: FontWeight.w400,
+          fontStyle: FontStyle.normal,
+        ),
+      );
 }
