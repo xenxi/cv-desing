484c672c72e32e67df312e7de19cb257e95ec38e
xenxi <antoniom.diaz.moreno@gmail.com>
2021-10-28 21:40:41 +0200

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,26 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'presentation/cv_desing_app.dart';
+import 'dart:js' as js;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  js.context["FIREBASE_API_KEY"] =
+      const String.fromEnvironment("FIREBASE_API_KEY");
+  js.context["FIREBASE_AUTH_DOMAIN"] =
+      const String.fromEnvironment("FIREBASE_AUTH_DOMAIN");
+  js.context["FIREBASE_PROJECT_ID"] =
+      const String.fromEnvironment("FIREBASE_PROJECT_ID");
+  js.context["FIREBASE_STORAGE_BUCKET"] =
+      const String.fromEnvironment("FIREBASE_STORAGE_BUCKET");
+  js.context["FIREBASE_MESSAGING_SENDER_ID"] =
+      const String.fromEnvironment("FIREBASE_MESSAGING_SENDER_ID");
+  js.context["FIREBASE_APP_ID"] =
+      const String.fromEnvironment("FIREBASE_APP_ID");
+  js.context["FIREBASE_MEASUREMENT_ID"] =
+      const String.fromEnvironment("FIREBASE_MEASUREMENT_ID");
 
   runApp(const CvDesingApp());
 }
