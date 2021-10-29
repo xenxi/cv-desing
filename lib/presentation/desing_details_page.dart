9a43efa4bcf384197c8708d620be8e1719ddecaa
xenxi <antoniom.diaz.moreno@gmail.com>
2021-10-29 07:36:50 +0200

    add page

---
+++ lib/presentation/desing_details_page.dart
@@ -0,0 +1,15 @@
+import 'package:cv_desing_website_flutter/domain/desing.dart';
+import 'package:flutter/material.dart';
+
+class DesingDetailsPage extends StatelessWidget {
+  const DesingDetailsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('hellow world'),
+      ),
+    );
+  }
+}
