2ec74e78386958a98e78db1684364085267b1850
xenxi <antoniom.diaz.moreno@gmail.com>
2021-10-29 07:59:36 +0200

    add image

--- lib/presentation/desing_details_page.dart
+++ lib/presentation/desing_details_page.dart
@@ -1,4 +1,6 @@
 import 'package:cv_desing_website_flutter/domain/desing.dart';
+import 'package:cv_desing_website_flutter/presentation/core/adaptative.dart';
+import 'package:cv_desing_website_flutter/presentation/home/widgets/section.dart';
 import 'package:flutter/material.dart';
 
 class DesingDetailsPage extends StatelessWidget {
@@ -8,9 +10,17 @@ class DesingDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final desing = ModalRoute.of(context)?.settings.arguments as Desing;
 
-    return Container(
-      child: Center(
-        child: Text(desing.reference),
+    return Scaffold(
+      appBar: AppBar(),
+      body: Section(
+        child: Image(
+          height: heightOfScreen(context),
+          alignment: Alignment.topCenter,
+          fit: BoxFit.fitHeight,
+          image: AssetImage(
+            desing.image(),
+          ),
+        ),
       ),
     );
   }
