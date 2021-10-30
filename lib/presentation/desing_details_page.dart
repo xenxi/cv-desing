bf7dbc8b1c02d4f634355d4e0fccbaf1c89aad56
xenxi <antoniom.diaz.moreno@gmail.com>
2021-10-30 12:34:01 +0200

    update mobile btn

--- lib/presentation/desing_details_page.dart
+++ lib/presentation/desing_details_page.dart
@@ -8,6 +8,7 @@ import 'package:cv_desing_website_flutter/presentation/core/custom_theme.dart';
 import 'package:cv_desing_website_flutter/presentation/shared/values/desing_data.dart';
 import 'package:cv_desing_website_flutter/presentation/shared/values/image_path.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 import 'shared/components/launcher_funtions.dart';
 import 'shared/values/email_address.dart';
@@ -22,45 +23,65 @@ class DesingDetailsPage extends StatelessWidget {
 
     if (desing == null) return notFound(context);
 
-    return Scaffold(
-      body: SizedBox(
-        height: heightOfScreen(context),
-        child: Stack(
-          children: [
-            Align(
-              alignment: Alignment.center,
-              child: _buildImage(desing),
-            ),
-            Align(
-              alignment: Alignment.topRight,
-              child: Padding(
-                padding: EdgeInsets.all(CustomTheme.defaultPadding),
-                child: ClipOval(
-                  child: Material(
-                    color: CustomTheme.primaryColor,
-                    child: IconButton(
-                      onPressed: () => close(context),
-                      icon: const Icon(Icons.close),
+    return ResponsiveBuilder(builder: (context, sizingInformation) {
+      return Scaffold(
+        body: SizedBox(
+          height: heightOfScreen(context),
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: _buildImage(desing),
+              ),
+              Align(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding: EdgeInsets.all(CustomTheme.defaultPadding),
+                  child: ClipOval(
+                    child: Material(
+                      color: CustomTheme.primaryColor,
+                      child: IconButton(
+                        onPressed: () => close(context),
+                        icon: const Icon(Icons.close),
+                      ),
                     ),
                   ),
                 ),
               ),
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Swing(
-          infinite: true,
-          child: const Icon(
-            Icons.shopping_cart_outlined,
-            color: Colors.black,
+              if (sizingInformation.isMobile)
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding:
+                        EdgeInsets.only(bottom: CustomTheme.defaultPadding),
+                    child: ElevatedButton.icon(
+                        onPressed: () =>
+                            openEmail(context, subject: desing.reference),
+                        icon: const Icon(
+                          Icons.shopping_cart_outlined,
+                          color: Colors.black,
+                        ),
+                        label: const Text(
+                          'Solicitar',
+                          style: TextStyle(color: Colors.black),
+                        )),
+                  ),
+                )
+            ],
           ),
         ),
-        backgroundColor: CustomTheme.primaryColor,
-        onPressed: () => openEmail(context, subject: desing.reference),
-      ),
-    );
+        floatingActionButton: sizingInformation.isMobile
+            ? null
+            : FloatingActionButton(
+                child: const Icon(
+                  Icons.shopping_cart_outlined,
+                  color: Colors.black,
+                ),
+                backgroundColor: CustomTheme.primaryColor,
+                onPressed: () => openEmail(context, subject: desing.reference),
+              ),
+      );
+    });
   }
 
   Widget _buildImage(Desing desing) {
