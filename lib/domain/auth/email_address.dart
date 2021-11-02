38fc66b478688e9e940d99216abd88e48878cadb
xenxi <antoniom.diaz.moreno@gmail.com>
2021-11-02 07:09:20 +0100

    add validation

--- lib/domain/auth/email_address.dart
+++ lib/domain/auth/email_address.dart
@@ -4,7 +4,7 @@ class EmailAddress {
     this.value,
   );
   factory EmailAddress(String input) {
-    return EmailAddress._(input);
+    return EmailAddress._(_validateEmailAddress(input));
   }
 
   @override
@@ -19,4 +19,19 @@ class EmailAddress {
 
   @override
   String toString() => 'EmailAddress($value)';
+
+  static String _validateEmailAddress(String input) {
+    const emailRegex =
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
+    if (RegExp(emailRegex).hasMatch(input)) return input;
+
+    throw InvalidEmailException(failedValue: input);
+  }
+}
+
+class InvalidEmailException implements Exception {
+  final String failedValue;
+  InvalidEmailException({
+    required this.failedValue,
+  });
 }
