5c062361a7c8847b35db48b31787c8c3076ad922
xenxi <antoniom.diaz.moreno@gmail.com>
2021-11-06 07:51:36 +0100

    :necktie: add auth failures

---
+++ lib/domain/auth/failures/auth_failures.dart
@@ -0,0 +1,11 @@
+import 'package:cv_desing_website_flutter/domain/failure.dart';
+
+abstract class AuthFailure implements Failure {}
+
+class CancelledByUser implements AuthFailure {}
+
+class ServerError implements AuthFailure {}
+
+class EmailAlreadyInUse implements AuthFailure {}
+
+class InvalidEmailAndPasswordCombination implements AuthFailure {}
