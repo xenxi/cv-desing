8aa0c7bb1a3cb1d08d63961899f5e946f4c8df53
xenxi <antoniom.diaz.moreno@gmail.com>
2021-11-07 09:41:28 +0100

    :white_check_mark: green test

--- lib/application/auth/sign_in_form/sign_in_form_bloc.dart
+++ lib/application/auth/sign_in_form/sign_in_form_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:cv_desing_website_flutter/domain/auth/failures/auth_failures.dart';
+import 'package:cv_desing_website_flutter/domain/auth/i_auth_facade.dart';
 import 'package:cv_desing_website_flutter/domain/auth/value_objects/email_address.dart';
 import 'package:cv_desing_website_flutter/domain/auth/value_objects/password.dart';
 import 'package:cv_desing_website_flutter/domain/failure.dart';
@@ -11,7 +12,8 @@ part 'sign_in_form_event.dart';
 part 'sign_in_form_state.dart';
 
 class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
-  SignInFormBloc() : super(SignInFormState.initial()) {
+  final IAuthFacade _authFacade;
+  SignInFormBloc(this._authFacade) : super(SignInFormState.initial()) {
     on<SignInFormEvent>((event, emit) {
       if (event is EmailChanged) {
         emit(SignInFormState(
@@ -23,6 +25,11 @@ class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
             email: state.email,
             password: Password.create(event.password),
             failureOrSuccessOption: none()));
+      } else if (event is SignInWithEmailAndPasswordPressed) {
+        emit(SignInFormState(
+            email: state.email,
+            password: state.password,
+            failureOrSuccessOption: some(right(unit))));
       }
     });
   }
--- test/application/auth/sign_in_form/sign_in_form_bloc_test.dart
+++ test/application/auth/sign_in_form/sign_in_form_bloc_test.dart
@@ -1,14 +1,24 @@
 import 'package:cv_desing_website_flutter/application/auth/sign_in_form/sign_in_form_bloc.dart';
+import 'package:cv_desing_website_flutter/domain/auth/i_auth_facade.dart';
 import 'package:cv_desing_website_flutter/domain/auth/value_objects/email_address.dart';
 import 'package:cv_desing_website_flutter/domain/auth/value_objects/password.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:bloc_test/bloc_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockIAuthFacade extends Mock implements IAuthFacade {}
 
 void main() {
+  late MockIAuthFacade authFacade;
+
+  setUp(() {
+    authFacade = MockIAuthFacade();
+  });
+
   group('signInForm should', () {
     test('be in empty state', () {
-      final bloc = SignInFormBloc();
+      final bloc = SignInFormBloc(authFacade);
 
       final expectedEmptyState = SignInFormState(
           email: EmailAddress.empty(),
@@ -17,7 +27,7 @@ void main() {
       expect(bloc.state, equals(expectedEmptyState));
     });
     blocTest<SignInFormBloc, SignInFormState>('update email when email changed',
-        build: () => SignInFormBloc(),
+        build: () => SignInFormBloc(authFacade),
         act: (bloc) {
           bloc.add(const EmailChanged('aGivenValidEmail@test.com'));
         },
@@ -30,7 +40,7 @@ void main() {
 
     blocTest<SignInFormBloc, SignInFormState>(
         'update password when password changed',
-        build: () => SignInFormBloc(),
+        build: () => SignInFormBloc(authFacade),
         act: (bloc) {
           bloc.add(const PasswordChanged('anyPassword'));
         },
@@ -43,16 +53,35 @@ void main() {
 
     blocTest<SignInFormBloc, SignInFormState>(
         'signIn user with email and password',
-        build: () => SignInFormBloc(),
+        build: () => SignInFormBloc(authFacade),
+        setUp: () {
+          when(
+            () {
+              final aValidEmail = EmailAddress.create('anyValidEmail@test.com');
+              final aValidPassword = Password.create('anyValidPassword');
+              return authFacade.signInWithEmailAndPassword(
+                  email: aValidEmail.getOrElse(() => throw Exception()),
+                  password: aValidPassword.getOrElse(() => throw Exception()));
+            },
+          ).thenAnswer((_) => Future.value(right(unit)));
+        },
         act: (bloc) {
+          bloc.add(const EmailChanged('anyValidEmail@test.com'));
           bloc.add(const PasswordChanged('anyValidPassword'));
-          bloc.add(const EmailChanged('anyValidEmail@gmail.com'));
           bloc.add(const SignInWithEmailAndPasswordPressed());
         },
         expect: () => <SignInFormState>[
               SignInFormState(
+                  email: EmailAddress.create('anyValidEmail@test.com'),
+                  password: Password.empty(),
+                  failureOrSuccessOption: none()),
+              SignInFormState(
                   password: Password.create('anyValidPassword'),
-                  email: EmailAddress.create('anyValidEmail@gmail.com'),
+                  email: EmailAddress.create('anyValidEmail@test.com'),
+                  failureOrSuccessOption: none()),
+              SignInFormState(
+                  password: Password.create('anyValidPassword'),
+                  email: EmailAddress.create('anyValidEmail@test.com'),
                   failureOrSuccessOption: some(right(unit)))
             ]);
   });
