03a3a4f756c144d9dee56c3d6c4b88eb2d4184d5
xenxi <antoniom.diaz.moreno@gmail.com>
2021-11-08 06:47:52 +0100

    :wrench: configure injections

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:cv_desing_website_flutter/presentation/core/dependency_injections/ioc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
 
 import 'presentation/cv_desing_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
+  configureInjection(Environment.prod);
   runApp(const CvDesingApp());
 }
--- lib/presentation/core/dependency_injections/ioc.config.dart
+++ lib/presentation/core/dependency_injections/ioc.config.dart
@@ -4,26 +4,30 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
-// ignore_for_file: require_trailing_commas
-
-import 'package:firebase_auth/firebase_auth.dart' as _i5;
+import 'package:firebase_auth/firebase_auth.dart' as _i3;
 import 'package:get_it/get_it.dart' as _i1;
-import 'package:google_sign_in/google_sign_in.dart' as _i6;
+import 'package:google_sign_in/google_sign_in.dart' as _i4;
 import 'package:injectable/injectable.dart' as _i2;
 
 import '../../../application/auth/sign_in_form/sign_in_form_bloc.dart' as _i7;
-import '../../../domain/auth/i_auth_facade.dart' as _i3;
-import '../../../infrastructure/auth/firebase_auth_facade.dart'
-    as _i4; // ignore_for_file: unnecessary_lambdas
+import '../../../domain/auth/i_auth_facade.dart' as _i5;
+import '../../../infrastructure/auth/firebase_auth_facade.dart' as _i6;
+import '../../../infrastructure/core/register_module.dart'
+    as _i8; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
 _i1.GetIt $initGetIt(_i1.GetIt get,
     {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
-  gh.lazySingleton<_i3.IAuthFacade>(() =>
-      _i4.FirebaseAuthFacade(get<_i5.FirebaseAuth>(), get<_i6.GoogleSignIn>()));
+  final registerModule = _$RegisterModule();
+  gh.lazySingleton<_i3.FirebaseAuth>(() => registerModule.firebaseAuth);
+  gh.lazySingleton<_i4.GoogleSignIn>(() => registerModule.googleSignIn);
+  gh.lazySingleton<_i5.IAuthFacade>(() =>
+      _i6.FirebaseAuthFacade(get<_i3.FirebaseAuth>(), get<_i4.GoogleSignIn>()));
   gh.factory<_i7.SignInFormBloc>(
-      () => _i7.SignInFormBloc(get<_i3.IAuthFacade>()));
+      () => _i7.SignInFormBloc(get<_i5.IAuthFacade>()));
   return get;
 }
+
+class _$RegisterModule extends _i8.RegisterModule {}
