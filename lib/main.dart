c4f1718d4928c2fc747a9ce3c81f0bed8ca9f620
xenxi <antoniom.diaz.moreno@gmail.com>
2022-03-26 12:27:38 +0100

    :rotating_light: organize imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:cv_desing_website_flutter/presentation/core/dependency_injections/ioc.dart';
+import 'package:cv_desing_website_flutter/presentation/cv_desing_app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
-
-import 'presentation/cv_desing_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
