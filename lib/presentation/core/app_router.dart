6e700d26e5fc37b44ceb076d8d784f4e58bd37ff
xenxi <antoniom.diaz.moreno@gmail.com>
2022-04-02 14:06:44 +0200

    :lipstick: add desings page

--- lib/presentation/core/app_router.dart
+++ lib/presentation/core/app_router.dart
@@ -1,12 +1,14 @@
 import 'package:cv_desing_website_flutter/presentation/auth/auth_page.dart';
 import 'package:cv_desing_website_flutter/presentation/coming_soon/coming_soon_page.dart';
 import 'package:cv_desing_website_flutter/presentation/desing_details/desing_details_page.dart';
+import 'package:cv_desing_website_flutter/presentation/desings/desings_page.dart';
 import 'package:cv_desing_website_flutter/presentation/home/home_page.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
   static String blog = '/blog';
   static String home = '/preview';
+  static String desings = '/desings';
   static String details = '/details';
   static String auth = '/auth';
   static Map<String, WidgetBuilder> routes = {
@@ -16,5 +18,6 @@ class AppRouter {
     blog: (context) => const ComingSoonPage(),
     auth: (context) => const AuthPage(),
     details: (context) => const DesingDetailsPage(),
+    desings: (context) => const DesingsPage(),
   };
 }
