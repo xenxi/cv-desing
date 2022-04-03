fd6d54aa599449b631f0b63d40122636e8870257
xenxi <antoniom.diaz.moreno@gmail.com>
2022-04-03 13:09:47 +0200

    :bricks: add navigator impl

---
+++ lib/presentation/core/routes/navigators/custom_navigator.dart
@@ -0,0 +1,17 @@
+import 'package:cv_desing_website_flutter/presentation/core/routes/navigators/i_navigator.dart';
+import 'package:flutter/material.dart';
+
+class CustomNavigator implements INavigator {
+  CustomNavigator(this.key);
+  final GlobalKey<NavigatorState> key;
+
+  @override
+  Future<void> navigateTo(String routeName) =>
+      key.currentState!.pushNamed(routeName);
+
+  @override
+  Future<void> goBack() {
+    key.currentState!.pop();
+    return Future.value();
+  }
+}
