aca2857df181ec5487898b7352bcde77417c8fc6
xenxi <antoniom.diaz.moreno@gmail.com>
2022-04-03 09:48:24 +0200

    🎨 update structure

--- lib/presentation/shared/components/adaptative_funtions.dart
+++ lib/presentation/shared/components/adaptative_funtions.dart
@@ -2,3 +2,23 @@ import 'package:flutter/material.dart';
 
 bool isMobileScreen(BuildContext context) =>
     MediaQuery.of(context).size.width < 720;
+
+double widthOfScreen(BuildContext context) => MediaQuery.of(context).size.width;
+double heightOfScreen(BuildContext context) =>
+    MediaQuery.of(context).size.height;
+
+double assignHeight({
+  required BuildContext context,
+  required double fraction,
+  double additions = 0,
+  double subs = 0,
+}) =>
+    (heightOfScreen(context) - subs + additions) * fraction;
+
+double assignWidth({
+  required BuildContext context,
+  required double fraction,
+  double additions = 0,
+  double subs = 0,
+}) =>
+    (widthOfScreen(context) - subs + additions) * fraction;
