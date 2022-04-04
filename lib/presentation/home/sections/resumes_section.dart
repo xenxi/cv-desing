413c9b23a07ead1158a0b168fa2272a3c7fa2c02
xenxi <antoniom.diaz.moreno@gmail.com>
2022-04-04 20:24:28 +0200

    :recycle: refact reduce indentation

--- lib/presentation/home/sections/resumes_section.dart
+++ lib/presentation/home/sections/resumes_section.dart
@@ -3,13 +3,11 @@ import 'package:cv_desing_website_flutter/presentation/core/app_router.dart';
 import 'package:cv_desing_website_flutter/presentation/core/custom_theme.dart';
 import 'package:cv_desing_website_flutter/presentation/cv_desing_app.dart';
 import 'package:cv_desing_website_flutter/presentation/home/widgets/featured_desings/featured_desings_list.dart';
-import 'package:cv_desing_website_flutter/presentation/home/widgets/section.dart';
 import 'package:cv_desing_website_flutter/presentation/home/widgets/show_more_button.dart';
 import 'package:cv_desing_website_flutter/presentation/shared/section_tittle.dart';
 import 'package:cv_desing_website_flutter/presentation/shared/values/desing_data.dart';
 import 'package:cv_desing_website_flutter/presentation/shared/values/location.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ResumesSection extends StatelessWidget {
   ResumesSection({Key? key}) : super(key: key);
@@ -20,25 +18,21 @@ class ResumesSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Section(
-      isMobile: false,
-      expandable: false,
-      child: Column(
-        children: [
-          SectionTitle(
-            title: Location.curriculums,
-            subTitle: Location.portfolioSectionSubtitle,
-            color: CustomTheme.primaryColor,
-            trailing: ShowMoreButton(
-              onPressed: () => Navigator.of(navigator.currentContext!)
-                  .pushNamed(AppRouter.desings),
-            ),
+    return Column(
+      children: [
+        SectionTitle(
+          title: Location.curriculums,
+          subTitle: Location.portfolioSectionSubtitle,
+          color: CustomTheme.primaryColor,
+          trailing: ShowMoreButton(
+            onPressed: () => Navigator.of(navigator.currentContext!)
+                .pushNamed(AppRouter.desings),
           ),
-          Expanded(
-            child: FeaturedDesingsList(desings: desings),
-          ),
-        ],
-      ),
+        ),
+        Expanded(
+          child: FeaturedDesingsList(desings: desings),
+        ),
+      ],
     );
   }
 }
