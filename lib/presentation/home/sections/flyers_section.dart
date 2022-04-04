17ad57d5894f65e00cec0457ef71b2c220680c1e
xenxi <antoniom.diaz.moreno@gmail.com>
2022-04-04 20:24:07 +0200

    :recycle: refact reduce indentation

--- lib/presentation/home/sections/flyers_section.dart
+++ lib/presentation/home/sections/flyers_section.dart
@@ -3,7 +3,6 @@ import 'package:cv_desing_website_flutter/presentation/core/app_router.dart';
 import 'package:cv_desing_website_flutter/presentation/core/custom_theme.dart';
 import 'package:cv_desing_website_flutter/presentation/cv_desing_app.dart';
 import 'package:cv_desing_website_flutter/presentation/home/widgets/featured_desings/featured_desings_list.dart';
-import 'package:cv_desing_website_flutter/presentation/home/widgets/section.dart';
 import 'package:cv_desing_website_flutter/presentation/home/widgets/show_more_button.dart';
 import 'package:cv_desing_website_flutter/presentation/shared/section_tittle.dart';
 import 'package:cv_desing_website_flutter/presentation/shared/values/desing_data.dart';
@@ -19,25 +18,21 @@ class FlyersSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Section(
-      isMobile: false,
-      expandable: false,
-      child: Column(
-        children: [
-          SectionTitle(
-            title: Location.flyers,
-            subTitle: Location.portfolioSectionSubtitle,
-            color: CustomTheme.primaryColor,
-            trailing: ShowMoreButton(
-              onPressed: () => Navigator.of(navigator.currentContext!)
-                  .pushNamed(AppRouter.desings),
-            ),
+    return Column(
+      children: [
+        SectionTitle(
+          title: Location.flyers,
+          subTitle: Location.portfolioSectionSubtitle,
+          color: CustomTheme.primaryColor,
+          trailing: ShowMoreButton(
+            onPressed: () => Navigator.of(navigator.currentContext!)
+                .pushNamed(AppRouter.desings),
           ),
-          Expanded(
-            child: FeaturedDesingsList(desings: desings),
-          ),
-        ],
-      ),
+        ),
+        Expanded(
+          child: FeaturedDesingsList(desings: desings),
+        ),
+      ],
     );
   }
 }
