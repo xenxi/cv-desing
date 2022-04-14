7c4c1cf198ef853fced522af3014ec0f5d8351c9
xenxi <antoniom.diaz.moreno@gmail.com>
2022-04-14 09:38:57 +0200

    :lipstick: add gradients

--- lib/presentation/core/custom_theme.dart
+++ lib/presentation/core/custom_theme.dart
@@ -2,6 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CustomTheme {
+  static const List<Color> snowGradient = [
+    Color.fromRGBO(255, 222, 242, 1),
+    Color.fromRGBO(230, 240, 255, 1),
+  ];
+  static const List<Color> fireGradient = [
+    Color.fromRGBO(255, 108, 135, 1),
+    Color.fromRGBO(252, 114, 70, 1),
+  ];
+  static const List<Color> batmanGradient = [
+    Color.fromRGBO(51, 51, 51, 1),
+    Color.fromRGBO(18, 18, 18, 1),
+  ];
+  static const List<Color> peachyGradient = [
+    Color.fromRGBO(255, 72, 182, 1),
+    Color.fromRGBO(255, 126, 108, 1),
+  ];
+  static const List<Color> pastelGradient = [
+    Color.fromRGBO(221, 149, 247, 1),
+    Color.fromRGBO(154, 173, 249, 1),
+    Color.fromRGBO(127, 201, 217, 1),
+  ];
+  static const List<Color> greenGradient = [
+    Color.fromRGBO(135, 207, 142, 1),
+    Color.fromRGBO(136, 207, 135, 1),
+  ];
+  static const List<Color> lemonGradient = [
+    Color.fromRGBO(251, 205, 56, 1),
+    Color.fromRGBO(252, 187, 91, 1),
+  ];
   static const double navbarHeight = 100.0;
   static const double navbarMobileHeight = 60.0;
   static const double defaultPadding = 20.0;
