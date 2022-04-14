835e2a1ac85138ab61f5c19ee493482206aed695
xenxi <antoniom.diaz.moreno@gmail.com>
2022-04-14 09:06:32 +0200

    :recycle: refact reduce indentation

--- lib/presentation/views/auth/auth_view.dart
+++ lib/presentation/views/auth/auth_view.dart
@@ -1,24 +1,13 @@
 import 'package:cv_desing_website_flutter/presentation/views/auth/sections/login/sign_in_section.dart';
 import 'package:flutter/material.dart';
-import 'package:responsive_builder/responsive_builder.dart';
 
 class AuthView extends StatelessWidget {
   const AuthView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ResponsiveBuilder(
-      builder: (context, sizingInformation) {
-        return FittedBox(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: const [
-              SignInSection(),
-            ],
-          ),
-        );
-      },
+    return const FittedBox(
+      child: SignInSection(),
     );
   }
 }
