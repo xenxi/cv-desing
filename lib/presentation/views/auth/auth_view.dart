82ff18bd70a4ac5c37e57786aef4b96cc3696263
xenxi <antoniom.diaz.moreno@gmail.com>
2022-04-14 09:00:58 +0200

    :fire: remove unnecessary code

--- lib/presentation/views/auth/auth_view.dart
+++ lib/presentation/views/auth/auth_view.dart
@@ -1,10 +1,4 @@
-import 'package:cv_desing_website_flutter/application/navigation/bloc/navigation_bloc.dart';
 import 'package:cv_desing_website_flutter/presentation/core/custom_theme.dart';
-import 'package:cv_desing_website_flutter/presentation/core/navbar/custom_mobile_navbar.dart';
-import 'package:cv_desing_website_flutter/presentation/core/navbar/custom_navbar.dart';
-import 'package:cv_desing_website_flutter/presentation/core/navbar/navbar_item_data.dart';
-import 'package:cv_desing_website_flutter/presentation/shared/values/location.dart';
-import 'package:cv_desing_website_flutter/presentation/shared/values/social_data.dart';
 import 'package:cv_desing_website_flutter/presentation/views/auth/sections/login/sign_in_mobile_section.dart';
 import 'package:cv_desing_website_flutter/presentation/views/auth/sections/login/sign_in_section.dart';
 import 'package:flutter/material.dart';
@@ -31,30 +25,6 @@ class AuthView extends StatelessWidget {
         );
       },
     );
-  }
-
-  Widget _buildNavBar(
-    BuildContext context, {
-    required SizingInformation sizingInformation,
-  }) {
-    return sizingInformation.isMobile
-        ? const CustomMobileNavBar(
-            loginButtonVisible: false,
-          )
-        : CustomNavBar(
-            socialData: SocialData.links,
-            navItems: [
-              NavItemData(
-                name: Location.home,
-                onTapEvent: const NavigateToHomeSelected(),
-                isSelected: true,
-              ),
-              NavItemData(
-                name: Location.desings,
-                onTapEvent: const NavigateToDesingsSelected(),
-              ),
-            ],
-          );
   }
 
   Widget _buildMobileSection(SizingInformation sizingInformation) =>
