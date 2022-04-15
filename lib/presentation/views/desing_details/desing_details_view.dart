32b920ecebb0dc0964f928e7435b77f77a5572f6
xenxi <antoniom.diaz.moreno@gmail.com>
2022-04-15 21:25:10 +0200

    :recycle: refact extract class

--- lib/presentation/views/desing_details/desing_details_view.dart
+++ lib/presentation/views/desing_details/desing_details_view.dart
@@ -1,12 +1,7 @@
 import 'package:cv_desing_website_flutter/application/desing_details/desing_details_bloc.dart';
 import 'package:cv_desing_website_flutter/application/navigation/navigation_bloc.dart';
-import 'package:cv_desing_website_flutter/domain/desing.dart';
-import 'package:cv_desing_website_flutter/presentation/core/custom_theme.dart';
 import 'package:cv_desing_website_flutter/presentation/core/dependency_injections/ioc.dart';
-import 'package:cv_desing_website_flutter/presentation/shared/components/adaptative_funtions.dart';
-import 'package:cv_desing_website_flutter/presentation/shared/widgets/close_button.dart';
-import 'package:cv_desing_website_flutter/presentation/views/desing_details/widgets/details_mockup.dart';
-import 'package:cv_desing_website_flutter/presentation/views/desing_details/widgets/request_button.dart';
+import 'package:cv_desing_website_flutter/presentation/views/desing_details/widgets/details_body.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -30,31 +25,8 @@ class DesingDetailsView extends StatelessWidget {
           },
           builder: (context, state) {
             if (state is LoadSuccess) {
-              return SizedBox(
-                height: heightOfScreen(context),
-                child: Stack(
-                  children: [
-                    DetailMockUp(child: _buildImage(state.desing)),
-                    const Align(
-                      alignment: Alignment.topRight,
-                      child: Padding(
-                        padding: EdgeInsets.all(CustomTheme.defaultPadding),
-                        child: RoundedCloseButton(),
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                          bottom: CustomTheme.defaultPadding,
-                        ),
-                        child: RequestButton(
-                          reference: state.desing.reference,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
+              return DetailsBody(
+                desing: state.desing,
               );
             }
 
@@ -62,17 +34,6 @@ class DesingDetailsView extends StatelessWidget {
               child: Text('Loading...'),
             );
           },
-        ),
-      );
-
-  Widget _buildImage(Desing desing) => Hero(
-        tag: desing.reference,
-        child: Image(
-          alignment: Alignment.topCenter,
-          fit: BoxFit.cover,
-          image: AssetImage(
-            desing.image(),
-          ),
         ),
       );
 }
---
+++ lib/presentation/views/desing_details/widgets/details_body.dart
@@ -0,0 +1,54 @@
+import 'package:cv_desing_website_flutter/domain/desing.dart';
+import 'package:cv_desing_website_flutter/presentation/core/custom_theme.dart';
+import 'package:cv_desing_website_flutter/presentation/shared/components/adaptative_funtions.dart';
+import 'package:cv_desing_website_flutter/presentation/shared/widgets/close_button.dart';
+import 'package:cv_desing_website_flutter/presentation/views/desing_details/widgets/details_mockup.dart';
+import 'package:cv_desing_website_flutter/presentation/views/desing_details/widgets/request_button.dart';
+import 'package:flutter/material.dart';
+
+class DetailsBody extends StatelessWidget {
+  const DetailsBody({
+    Key? key,
+    required this.desing,
+  }) : super(key: key);
+  final Desing desing;
+
+  @override
+  Widget build(BuildContext context) => SizedBox(
+        height: heightOfScreen(context),
+        child: Stack(
+          children: [
+            DetailMockUp(child: _buildImage(desing)),
+            const Align(
+              alignment: Alignment.topRight,
+              child: Padding(
+                padding: EdgeInsets.all(CustomTheme.defaultPadding),
+                child: RoundedCloseButton(),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  bottom: CustomTheme.defaultPadding,
+                ),
+                child: RequestButton(
+                  reference: desing.reference,
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+
+  Widget _buildImage(Desing desing) => Hero(
+        tag: desing.reference,
+        child: Image(
+          alignment: Alignment.topCenter,
+          fit: BoxFit.cover,
+          image: AssetImage(
+            desing.image(),
+          ),
+        ),
+      );
+}
