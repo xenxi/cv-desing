149d44e13aa582b14fbf42118fcb0883be2ff6ca
xenxi <antoniom.diaz.moreno@gmail.com>
2022-04-16 08:32:35 +0200

    :recycle: refact introduce fields

--- test/infrastructure/in_memory_desings_test.dart
+++ test/infrastructure/in_memory_desings_test.dart
@@ -1,3 +1,4 @@
+import 'package:cv_desing_website_flutter/domain/desing.dart';
 import 'package:cv_desing_website_flutter/domain/desing_failures.dart';
 import 'package:cv_desing_website_flutter/infrastructure/in_memory_desings.dart';
 import 'package:dartz/dartz.dart';
@@ -7,27 +8,24 @@ import '../domain/desing_mother.dart';
 
 void main() {
   group('InMemoryDesings should', () {
+    late Desing anyDesing;
+    late InMemoryDesings desings;
+    setUp(() {
+      anyDesing = DesingMother.random();
+      desings = InMemoryDesings([anyDesing]);
+    });
     test('found desing by reference', () async {
-      final anyDesing = DesingMother.random();
-      final desings = InMemoryDesings([anyDesing]);
-
       final result = await desings.getByReference(anyDesing.reference);
 
       expect(result, Right(anyDesing));
     });
     test('search by reference case insensitive', () async {
-      final anyDesing = DesingMother.random();
-      final desings = InMemoryDesings([anyDesing]);
-
       final result =
           await desings.getByReference(anyDesing.reference.toUpperCase());
 
       expect(result, Right(anyDesing));
     });
     test('return failure when not found desing by reference', () async {
-      final anyDesing = DesingMother.random();
-      final desings = InMemoryDesings([anyDesing]);
-
       final result = await desings.getByReference('unknownReference');
 
       expect(result, const Left(DesingNotFound('unknownReference')));
