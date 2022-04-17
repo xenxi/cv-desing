c7dbeba3a95da404829ee93916c834d7d6f3b8f4
xenxi <antoniom.diaz.moreno@gmail.com>
2022-04-17 21:26:51 +0200

    :sparkles: add editor view

--- lib/presentation/core/routes/app_router.dart
+++ lib/presentation/core/routes/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:cv_desing_website_flutter/presentation/views/auth/auth_view.dart';
 import 'package:cv_desing_website_flutter/presentation/views/coming_soon/coming_soon_view.dart';
 import 'package:cv_desing_website_flutter/presentation/views/desing_details/desing_details_view.dart';
+import 'package:cv_desing_website_flutter/presentation/views/desing_editor/desing_editor_view.dart';
 import 'package:cv_desing_website_flutter/presentation/views/desings/desings_view.dart';
 import 'package:cv_desing_website_flutter/presentation/views/home/home_view.dart';
 import 'package:dartz/dartz.dart';
@@ -13,12 +14,14 @@ class AppRouter {
   static const String desings = '/desings';
   static const String details = '/desings/:reference';
   static const String auth = '/auth';
+  static const String editor = '/editor';
   static Map<String, WidgetBuilder> routes = {
     // '': (context) => const ComingSoonPage(),
     initial: (context) => const HomeView(),
     home: (context) => const HomeView(),
     blog: (context) => const ComingSoonView(),
     auth: (context) => const AuthView(),
+    editor: (context) => const DesingEditorView(),
     details: (context) => const DesingDetailsView(reference: ''),
     desings: (context) => const DesingsView(),
   };
@@ -28,6 +31,7 @@ class AppRouter {
     home: (_) => const HomeView(),
     blog: (_) => const ComingSoonView(),
     auth: (_) => const AuthView(),
+    editor: (context) => const DesingEditorView(),
     details: (params) {
       final referenceOption = optionOf(params['reference']?.first);
       return referenceOption.fold(
