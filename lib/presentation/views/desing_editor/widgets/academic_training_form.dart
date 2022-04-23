1c9a731e502cd642f85455fdec0dac635d989d50
xenxi <antoniom.diaz.moreno@gmail.com>
2022-04-23 21:44:45 +0200

    :lipstick: update

--- lib/presentation/views/desing_editor/widgets/academic_training_form.dart
+++ lib/presentation/views/desing_editor/widgets/academic_training_form.dart
@@ -8,6 +8,10 @@ class AcademicTrainingForm extends StatelessWidget {
     return Form(
         child: Column(
       children: [
+        ElevatedButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+            label: const Text('Add')),
         TextFormField(
           decoration: const InputDecoration(
             prefixIcon: Icon(Icons.science_outlined),
@@ -20,24 +24,35 @@ class AcademicTrainingForm extends StatelessWidget {
             labelText: 'Escuela, instito o universidad',
           ),
         ),
-        TextFormField(
-          enableInteractiveSelection: false,
-          keyboardType: TextInputType.datetime,
-          decoration: const InputDecoration(
-            prefixIcon: Icon(Icons.insert_invitation_outlined),
-            labelText: 'Desde',
-          ),
-          onTap: () => _selectDate(context),
-        ),
-        TextFormField(
-          enableInteractiveSelection: false,
-          keyboardType: TextInputType.datetime,
-          decoration: const InputDecoration(
-            prefixIcon: Icon(Icons.date_range_rounded),
-            labelText: 'Hasta',
-          ),
-          onTap: () => _selectDate(context),
-        ),
+        Row(
+          children: [
+            Expanded(
+              child: TextFormField(
+                enableInteractiveSelection: false,
+                keyboardType: TextInputType.datetime,
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.date_range_rounded),
+                  labelText: 'Desde',
+                ),
+                onTap: () => _selectDate(context),
+              ),
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            Expanded(
+              child: TextFormField(
+                enableInteractiveSelection: false,
+                keyboardType: TextInputType.datetime,
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.date_range_rounded),
+                  labelText: 'Hasta',
+                ),
+                onTap: () => _selectDate(context),
+              ),
+            ),
+          ],
+        )
       ],
     ));
   }
