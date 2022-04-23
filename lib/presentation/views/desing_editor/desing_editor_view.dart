e51cd4575aa8523ed8230a7b5114f46ecd28112a
xenxi <antoniom.diaz.moreno@gmail.com>
2022-04-23 08:11:05 +0200

    :lipstick: add stepper

--- lib/presentation/views/desing_editor/desing_editor_view.dart
+++ lib/presentation/views/desing_editor/desing_editor_view.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class DesingEditorView extends StatelessWidget {
+class DesingEditorView extends HookWidget {
   const DesingEditorView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stepper(steps: [
+      Step(
+        title: const Text('Información personal y contacto'),
+        content: Container(
+            alignment: Alignment.centerLeft,
+            child: const Text('Content for Step 1')),
+      ),
+      const Step(
+        title: Text('Formación académica'),
+        content: Text('Content for Step 2'),
+      ),
+      const Step(
+        title: Text('Formaciones complementarias'),
+        content: Text('Content for Step 2'),
+      ),
+      const Step(
+        title: Text('Experiencia laboral'),
+        content: Text('Content for Step 2'),
+      ),
+      const Step(
+        title: Text('Idiomas'),
+        content: Text('Content for Step 2'),
+      ),
+      const Step(
+        title: Text('Software y Tecnologías'),
+        content: Text('Content for Step 2'),
+      ),
+      const Step(
+        title: Text('Competencias y habilidades'),
+        content: Text('Content for Step 2'),
+      ),
+    ]);
   }
 }
