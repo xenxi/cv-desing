9de842b4bc2702b85b80a58615e47a160eb4aad0
xenxi <antoniom.diaz.moreno@gmail.com>
2022-04-23 20:28:36 +0200

    :recycle: refact extract class

--- lib/presentation/views/desing_editor/desing_editor_view.dart
+++ lib/presentation/views/desing_editor/desing_editor_view.dart
@@ -2,10 +2,10 @@ import 'package:cv_desing_website_flutter/application/editor/cv_editor_bloc.dart
 import 'package:cv_desing_website_flutter/application/editor/sections.dart';
 import 'package:cv_desing_website_flutter/presentation/core/dependency_injections/ioc.dart';
 import 'package:cv_desing_website_flutter/presentation/shared/values/location.dart';
+import 'package:cv_desing_website_flutter/presentation/views/desing_editor/widgets/personal_info_form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class DesingEditorView extends HookWidget {
   const DesingEditorView({Key? key}) : super(key: key);
@@ -17,67 +17,40 @@ class DesingEditorView extends HookWidget {
       child: BlocBuilder<CveditorBloc, CveditorState>(
         builder: (context, state) {
           return Stepper(
-              currentStep: Section.values.indexOf(state.section),
-              onStepTapped: (index) => BlocProvider.of<CveditorBloc>(context)
-                  .add(SectionChanged(section: Section.values[index])),
-              steps: [
-                Step(
-                  title: const Text(Location.personalInformation),
-                  content: Form(
-                    child: Column(children: [
-                      const CircleAvatar(
-                        radius: 50,
-                        child: Icon(Icons.account_box_outlined, size: 60),
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          prefixIcon: Icon(Icons.person_outlined),
-                          labelText: 'Tu nombre',
-                        ),
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          prefixIcon: Icon(Icons.work_outline),
-                          labelText: 'Tu profesión o actividad',
-                        ),
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          prefixIcon: Icon(Icons.info_outline),
-                          labelText: 'Algo sobre ti',
-                        ),
-                        minLines: 4,
-                        keyboardType: TextInputType.multiline,
-                        maxLines: null,
-                      )
-                    ]),
-                  ),
-                ),
-                const Step(
-                  title: Text(Location.academicTraining),
-                  content: Text('Content for Step 2'),
-                ),
-                const Step(
-                  title: Text(Location.complementaryFormations),
-                  content: Text('Content for Step 2'),
-                ),
-                const Step(
-                  title: Text(Location.workExperience),
-                  content: Text('Content for Step 2'),
-                ),
-                const Step(
-                  title: Text(Location.languages),
-                  content: Text('Content for Step 2'),
-                ),
-                const Step(
-                  title: Text(Location.softwareSkills),
-                  content: Text('Content for Step 2'),
-                ),
-                const Step(
-                  title: Text(Location.skillsandAptitudes),
-                  content: Text('Content for Step 2'),
-                ),
-              ]);
+            currentStep: Section.values.indexOf(state.section),
+            onStepTapped: (index) => BlocProvider.of<CveditorBloc>(context)
+                .add(SectionChanged(section: Section.values[index])),
+            steps: const [
+              Step(
+                title: Text(Location.personalInformation),
+                content: PersonalInfoForm(),
+              ),
+              Step(
+                title: Text(Location.academicTraining),
+                content: Text('Content for Step 2'),
+              ),
+              Step(
+                title: Text(Location.complementaryFormations),
+                content: Text('Content for Step 2'),
+              ),
+              Step(
+                title: Text(Location.workExperience),
+                content: Text('Content for Step 2'),
+              ),
+              Step(
+                title: Text(Location.languages),
+                content: Text('Content for Step 2'),
+              ),
+              Step(
+                title: Text(Location.softwareSkills),
+                content: Text('Content for Step 2'),
+              ),
+              Step(
+                title: Text(Location.skillsandAptitudes),
+                content: Text('Content for Step 2'),
+              ),
+            ],
+          );
         },
       ),
     );
---
+++ lib/presentation/views/desing_editor/widgets/personal_info_form.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class PersonalInfoForm extends StatelessWidget {
+  const PersonalInfoForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(children: [
+        const CircleAvatar(
+          radius: 50,
+          child: Icon(Icons.account_box_outlined, size: 60),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            prefixIcon: Icon(Icons.person_outlined),
+            labelText: 'Tu nombre',
+          ),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            prefixIcon: Icon(Icons.work_outline),
+            labelText: 'Tu profesión o actividad',
+          ),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            prefixIcon: Icon(Icons.info_outline),
+            labelText: 'Algo sobre ti',
+          ),
+          minLines: 4,
+          keyboardType: TextInputType.multiline,
+          maxLines: null,
+        )
+      ]),
+    );
+  }
+}
