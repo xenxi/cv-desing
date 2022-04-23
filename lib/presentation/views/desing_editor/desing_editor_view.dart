7d82b9bf20239aa587e62f2c5d9d66000260d474
xenxi <antoniom.diaz.moreno@gmail.com>
2022-04-23 20:26:15 +0200

    :sparkles: add personal info form

--- lib/presentation/views/desing_editor/desing_editor_view.dart
+++ lib/presentation/views/desing_editor/desing_editor_view.dart
@@ -5,6 +5,7 @@ import 'package:cv_desing_website_flutter/presentation/shared/values/location.da
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class DesingEditorView extends HookWidget {
   const DesingEditorView({Key? key}) : super(key: key);
@@ -22,9 +23,35 @@ class DesingEditorView extends HookWidget {
               steps: [
                 Step(
                   title: const Text(Location.personalInformation),
-                  content: Container(
-                      alignment: Alignment.centerLeft,
-                      child: const Text('Content for Step 1')),
+                  content: Form(
+                    child: Column(children: [
+                      const CircleAvatar(
+                        radius: 50,
+                        child: Icon(Icons.account_box_outlined, size: 60),
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.person_outlined),
+                          labelText: 'Tu nombre',
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.work_outline),
+                          labelText: 'Tu profesión o actividad',
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.info_outline),
+                          labelText: 'Algo sobre ti',
+                        ),
+                        minLines: 4,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: null,
+                      )
+                    ]),
+                  ),
                 ),
                 const Step(
                   title: Text(Location.academicTraining),
