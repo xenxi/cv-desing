46c39b9e2aa63795d494ceba2616a8b001f0ea25
xenxi <antoniom.diaz.moreno@gmail.com>
2022-04-24 20:35:53 +0200

    :lipstick: update form

--- lib/presentation/views/desing_editor/widgets/skills_form.dart
+++ lib/presentation/views/desing_editor/widgets/skills_form.dart
@@ -1,34 +1,48 @@
+import 'package:cv_desing_website_flutter/application/editor/cv_editor_bloc.dart';
 import 'package:cv_desing_website_flutter/presentation/shared/widgets/price_chip.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SkillsForm extends StatelessWidget {
   const SkillsForm({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Expanded(
-          child: TextField(
-            decoration: const InputDecoration(
-              prefixIcon: Icon(Icons.language_outlined),
-              labelText: 'Idioma',
+    final controller = TextEditingController();
+    final focusNode = FocusNode();
+    return BlocBuilder<CveditorBloc, CveditorState>(
+      buildWhen: (previous, current) => previous.skills != current.skills,
+      builder: (context, state) {
+        return Row(
+          children: [
+            Expanded(
+              child: TextField(
+                focusNode: focusNode,
+                controller: controller,
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.language_outlined),
+                  labelText: 'Competencia o habilidad',
+                ),
+                onSubmitted: (skill) {
+                  controller.text = '';
+                  FocusScope.of(context).requestFocus(focusNode);
+                  BlocProvider.of<CveditorBloc>(context)
+                      .add(SkillAdded(skill: skill));
+                },
+              ),
             ),
-          ),
-        ),
-        SizedBox(width: 8),
-        Expanded(
-          child: Wrap(
-            children: [
-              InputChip(label: Text('Ejemplo')),
-              InputChip(label: Text('Ejemplo'))
-            ],
-          ),
-        )
-      ],
+            const SizedBox(width: 8),
+            Expanded(
+              child: Wrap(
+                children: [
+                  ...state.skills.map((skill) => InputChip(label: Text(skill)))
+                ],
+              ),
+            )
+          ],
+        );
+      },
     );
   }
 }
