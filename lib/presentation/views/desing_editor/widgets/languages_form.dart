a38fe36335878f4df6c11532bb477e447cd3b24f
xenxi <antoniom.diaz.moreno@gmail.com>
2022-04-30 20:54:06 +0200

    :recycle: refact extract class

--- lib/presentation/views/desing_editor/widgets/languages_form.dart
+++ lib/presentation/views/desing_editor/widgets/languages_form.dart
@@ -7,34 +7,42 @@ class LanguagesForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return Form(
       child: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.language_outlined),
-                    labelText: 'Idioma',
-                  ),
-                ),
-              ),
-              Expanded(
-                child: DropdownButton<String>(
-                  hint: const Text('Nivel'),
-                  items: const ['Nativo', 'Intermedio', 'Básico']
-                      .map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (String? value) {},
-                ),
-              )
-            ],
-          )
-        ],
+        children: [_Editor()],
       ),
+    );
+  }
+}
+
+class _Editor extends StatelessWidget {
+  const _Editor({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: TextFormField(
+            decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.language_outlined),
+              labelText: 'Idioma',
+            ),
+          ),
+        ),
+        Expanded(
+          child: DropdownButton<String>(
+            hint: const Text('Nivel'),
+            items: const ['Nativo', 'Intermedio', 'Básico'].map((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            onChanged: (String? value) {},
+          ),
+        )
+      ],
     );
   }
 }
