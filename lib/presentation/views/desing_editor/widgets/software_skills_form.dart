5ce2da5a6c1556dbd9bcf2ed94100598492bb242
xenxi <antoniom.diaz.moreno@gmail.com>
2022-04-30 14:06:05 +0200

    :recycle: extract method

--- lib/presentation/views/desing_editor/widgets/software_skills_form.dart
+++ lib/presentation/views/desing_editor/widgets/software_skills_form.dart
@@ -12,29 +12,40 @@ class SoftwareSkillsForm extends StatelessWidget {
           icon: const Icon(Icons.add),
           label: const Text('Añadir'),
         ),
-        Row(
-          children: [
-            Expanded(
-              child: TextField(
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.featured_play_list_outlined),
-                    labelText: 'Software o tecnología',
-                    helperText: 'Pulse enter para agregar'),
-                onSubmitted: (skill) {
-                  print(skill);
-                },
-              ),
-            ),
-            const SizedBox(width: 8),
-            Expanded(
-              child: Slider(
-                onChanged: (double value) {},
-                value: 0,
-                max: 100,
-              ),
-            )
-          ],
+        const _Editor(),
+      ],
+    );
+  }
+}
+
+class _Editor extends StatelessWidget {
+  const _Editor({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.featured_play_list_outlined),
+                labelText: 'Software o tecnología',
+                helperText: 'Pulse enter para agregar'),
+            onSubmitted: (skill) {
+              print(skill);
+            },
+          ),
         ),
+        const SizedBox(width: 8),
+        Expanded(
+          child: Slider(
+            onChanged: (double value) {},
+            value: 0,
+            max: 100,
+          ),
+        )
       ],
     );
   }
