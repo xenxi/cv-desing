466aff4714df199fee04ba28365dd5b46cf81f23
xenxi <antoniom.diaz.moreno@gmail.com>
2022-04-30 20:05:24 +0200

    :fire: remove unnecessary code

--- lib/application/editor/software_skills_form/software_skills_form_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'software_skills_form_bloc.dart';
-
-abstract class SoftwareSkillsFormEvent extends Equatable {
-  const SoftwareSkillsFormEvent();
-
-  @override
-  List<Object> get props => [];
-}
