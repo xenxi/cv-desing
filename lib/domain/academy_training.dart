0bc138a3509432d43c459c740aac414516def76f
xenxi <antoniom.diaz.moreno@gmail.com>
2022-05-01 08:17:55 +0200

    :necktie: add academy training

---
+++ lib/domain/academy_training.dart
@@ -0,0 +1,36 @@
+import 'package:cv_desing_website_flutter/domain/value_object.dart';
+
+class AcademyTraining extends ValueObject {
+  const AcademyTraining({
+    required this.title,
+    required this.schoold,
+    required this.since,
+    this.until,
+  });
+  final String title;
+  final String schoold;
+  final DateTime since;
+  final DateTime? until;
+
+  @override
+  List<Object?> get props => [
+        title,
+        schoold,
+      ];
+}
+
+class AcademyTrainings extends ValueObject {
+  factory AcademyTrainings.empty() => const AcademyTrainings([]);
+  const AcademyTrainings(this.value);
+
+  final List<AcademyTraining> value;
+  @override
+  List<Object?> get props => [...value];
+
+  AcademyTrainings add(AcademyTraining skill) =>
+      AcademyTrainings(List<AcademyTraining>.from(value)..add(skill));
+
+  AcademyTrainings remove(AcademyTraining skill) => AcademyTrainings(
+        List<AcademyTraining>.from(value)..remove(skill),
+      );
+}
