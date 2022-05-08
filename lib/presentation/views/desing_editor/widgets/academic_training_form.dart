9863bd21ed1658c3250bbdb2adba89b81c22c655
xenxi <antoniom.diaz.moreno@gmail.com>
2022-05-08 18:11:32 +0200

    :recycle: refact extract method

--- lib/presentation/views/desing_editor/widgets/academic_training_form.dart
+++ lib/presentation/views/desing_editor/widgets/academic_training_form.dart
@@ -10,59 +10,61 @@ class AcademicTrainingForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => AcademyTrainingFormBloc(),
-      child: BlocConsumer<AcademyTrainingFormBloc, AcademyTrainingFormState>(
-        listenWhen: (previous, current) =>
-            previous.saveFailureOrSuccessOption !=
-            current.saveFailureOrSuccessOption,
-        listener: (context, state) {
-          state.saveFailureOrSuccessOption.fold(
-            () => {},
-            (a) => BlocProvider.of<CvEditorBloc>(context)
-                .add(AcademyTrainingAdded(state.academyTraining)),
-          );
-        },
-        builder: (context, state) {
-          return Form(
-              child: Column(
-            children: [
-              ElevatedButton.icon(
-                onPressed: () =>
-                    BlocProvider.of<AcademyTrainingFormBloc>(context)
-                        .add(Saved()),
-                icon: const Icon(Icons.add),
-                label: const Text('Añadir'),
-              ),
-              CustomFormField(
-                text: 'Titulo',
-                icon: Icons.science_outlined,
-                value: state.academyTraining.title,
-                onChanged: (val) =>
-                    BlocProvider.of<AcademyTrainingFormBloc>(context).add(
-                  TitleChanged(val),
-                ),
-              ),
-              CustomFormField(
-                text: 'Escuela, instito o universidad',
-                icon: Icons.school_outlined,
-                value: state.academyTraining.schoold,
-                onChanged: (val) =>
-                    BlocProvider.of<AcademyTrainingFormBloc>(context).add(
-                  SchooldChanged(val),
-                ),
-              ),
-              CustomDateRangePicker(
-                dateRange: state.academyTraining.dateRange,
-                onChanged: (start, end) =>
-                    BlocProvider.of<AcademyTrainingFormBloc>(context).add(
-                  DateRangeChanged(since: start, until: end),
-                ),
-              ),
-            ],
-          ));
-        },
-      ),
-    );
+    return _buildForm();
   }
+
+  Widget _buildForm() => BlocProvider(
+        create: (context) => AcademyTrainingFormBloc(),
+        child: BlocConsumer<AcademyTrainingFormBloc, AcademyTrainingFormState>(
+          listenWhen: (previous, current) =>
+              previous.saveFailureOrSuccessOption !=
+              current.saveFailureOrSuccessOption,
+          listener: (context, state) {
+            state.saveFailureOrSuccessOption.fold(
+              () => {},
+              (a) => BlocProvider.of<CvEditorBloc>(context)
+                  .add(AcademyTrainingAdded(state.academyTraining)),
+            );
+          },
+          builder: (context, state) {
+            return Form(
+                child: Column(
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () =>
+                      BlocProvider.of<AcademyTrainingFormBloc>(context)
+                          .add(Saved()),
+                  icon: const Icon(Icons.add),
+                  label: const Text('Añadir'),
+                ),
+                CustomFormField(
+                  text: 'Titulo',
+                  icon: Icons.science_outlined,
+                  value: state.academyTraining.title,
+                  onChanged: (val) =>
+                      BlocProvider.of<AcademyTrainingFormBloc>(context).add(
+                    TitleChanged(val),
+                  ),
+                ),
+                CustomFormField(
+                  text: 'Escuela, instito o universidad',
+                  icon: Icons.school_outlined,
+                  value: state.academyTraining.schoold,
+                  onChanged: (val) =>
+                      BlocProvider.of<AcademyTrainingFormBloc>(context).add(
+                    SchooldChanged(val),
+                  ),
+                ),
+                CustomDateRangePicker(
+                  dateRange: state.academyTraining.dateRange,
+                  onChanged: (start, end) =>
+                      BlocProvider.of<AcademyTrainingFormBloc>(context).add(
+                    DateRangeChanged(since: start, until: end),
+                  ),
+                ),
+              ],
+            ));
+          },
+        ),
+      );
 }
