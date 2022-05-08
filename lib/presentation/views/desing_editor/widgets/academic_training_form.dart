ee6ac46576ceed4160eac03a1d7e3af5e9864ac8
xenxi <antoniom.diaz.moreno@gmail.com>
2022-05-08 18:28:04 +0200

    :lipstick: update form

--- lib/presentation/views/desing_editor/widgets/academic_training_form.dart
+++ lib/presentation/views/desing_editor/widgets/academic_training_form.dart
@@ -1,5 +1,6 @@
 import 'package:cv_desing_website_flutter/application/editor/academy_training_form/academy_training_form_bloc.dart';
 import 'package:cv_desing_website_flutter/application/editor/cv_editor_bloc.dart';
+import 'package:cv_desing_website_flutter/domain/academy_training.dart';
 import 'package:cv_desing_website_flutter/presentation/shared/widgets/custom_date_range_picker.dart';
 import 'package:cv_desing_website_flutter/presentation/shared/widgets/custom_form_field.dart';
 import 'package:flutter/material.dart';
@@ -10,7 +11,17 @@ class AcademicTrainingForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _buildForm();
+    return BlocBuilder<CvEditorBloc, CvEditorState>(
+      builder: (context, state) {
+        return Column(
+          children: [
+            _buildForm(),
+            ...state.academyTrainings.value.map(
+                (academyTraining) => _buildAcademyTraining(academyTraining)),
+          ],
+        );
+      },
+    );
   }
 
   Widget _buildForm() => BlocProvider(
@@ -22,21 +33,16 @@ class AcademicTrainingForm extends StatelessWidget {
           listener: (context, state) {
             state.saveFailureOrSuccessOption.fold(
               () => {},
-              (a) => BlocProvider.of<CvEditorBloc>(context)
-                  .add(AcademyTrainingAdded(state.academyTraining)),
+              (failureOrSucces) => failureOrSucces.fold(
+                  (l) => {},
+                  (_) => BlocProvider.of<CvEditorBloc>(context)
+                      .add(AcademyTrainingAdded(state.academyTraining))),
             );
           },
           builder: (context, state) {
             return Form(
                 child: Column(
               children: [
-                ElevatedButton.icon(
-                  onPressed: () =>
-                      BlocProvider.of<AcademyTrainingFormBloc>(context)
-                          .add(Saved()),
-                  icon: const Icon(Icons.add),
-                  label: const Text('Añadir'),
-                ),
                 CustomFormField(
                   text: 'Titulo',
                   icon: Icons.science_outlined,
@@ -62,9 +68,19 @@ class AcademicTrainingForm extends StatelessWidget {
                     DateRangeChanged(since: start, until: end),
                   ),
                 ),
+                const SizedBox(height: 20),
+                ElevatedButton.icon(
+                  onPressed: () =>
+                      BlocProvider.of<AcademyTrainingFormBloc>(context)
+                          .add(Saved()),
+                  icon: const Icon(Icons.add_outlined),
+                  label: const Text('Añadir'),
+                ),
               ],
             ));
           },
         ),
       );
+
+  Widget _buildAcademyTraining(AcademyTraining academyTraining) => _buildForm();
 }
