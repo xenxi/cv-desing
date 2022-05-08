434ddae9fceeb9d1890ef118951d974cf0ae459b
xenxi <antoniom.diaz.moreno@gmail.com>
2022-05-08 07:02:40 +0200

    :lipstick: update form

--- lib/presentation/views/desing_editor/widgets/academic_training_form.dart
+++ lib/presentation/views/desing_editor/widgets/academic_training_form.dart
@@ -1,45 +1,61 @@
+import 'package:cv_desing_website_flutter/application/editor/academy_training_form/academy_training_form_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AcademicTrainingForm extends StatelessWidget {
   const AcademicTrainingForm({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-        child: Column(
-      children: [
-        ElevatedButton.icon(
-          onPressed: () {},
-          icon: const Icon(Icons.add),
-          label: const Text('Añadir'),
-        ),
-        TextFormField(
-          decoration: const InputDecoration(
-            prefixIcon: Icon(Icons.science_outlined),
-            labelText: 'Titulo',
-          ),
-        ),
-        TextFormField(
-          decoration: const InputDecoration(
-            prefixIcon: Icon(Icons.school_outlined),
-            labelText: 'Escuela, instito o universidad',
-          ),
-        ),
-        Row(
+    return BlocConsumer<AcademyTrainingFormBloc, AcademyTrainingFormState>(
+      listener: (context, state) {
+        // TODO: implement listener
+      },
+      builder: (context, state) {
+        return Form(
+            child: Column(
           children: [
-            Expanded(
-              child: _datePicker(context, text: 'Desde'),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+              label: const Text('Añadir'),
             ),
-            const SizedBox(
-              width: 8,
+            TextFormField(
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.science_outlined),
+                labelText: 'Titulo',
+              ),
+              autocorrect: false,
+              onChanged: (val) =>
+                  BlocProvider.of<AcademyTrainingFormBloc>(context).add(
+                TitleChanged(val),
+              ),
+              validator: (_) => state.academyTraining.title
+                  .fold((l) => 'Titulo invalido', (r) => null),
             ),
-            Expanded(
-              child: _datePicker(context, text: 'Hasta'),
+            TextFormField(
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.school_outlined),
+                labelText: 'Escuela, instito o universidad',
+              ),
             ),
+            Row(
+              children: [
+                Expanded(
+                  child: _datePicker(context, text: 'Desde'),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Expanded(
+                  child: _datePicker(context, text: 'Hasta'),
+                ),
+              ],
+            )
           ],
-        )
-      ],
-    ));
+        ));
+      },
+    );
   }
 
   Widget _datePicker(BuildContext context, {required String text}) =>
