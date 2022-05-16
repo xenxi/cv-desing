24c2a28a6feda95f071286ec9b00841893bba636
xenxi <antoniom.diaz.moreno@gmail.com>
2022-05-16 07:17:01 +0200

    :coffin: remove dead code

--- lib/presentation/views/desing_editor/widgets/complementary_formations_form.dart
+++ lib/presentation/views/desing_editor/widgets/complementary_formations_form.dart
@@ -1,12 +1,10 @@
 import 'package:cv_desing_website_flutter/application/editor/complementary_training_form/complementary_training_form_bloc.dart';
 import 'package:cv_desing_website_flutter/application/editor/cv_editor_bloc.dart';
 import 'package:cv_desing_website_flutter/domain/complementary_training.dart';
-import 'package:cv_desing_website_flutter/domain/value_objects/course_hours.dart';
 import 'package:cv_desing_website_flutter/presentation/shared/widgets/custom_date_range_picker.dart';
 import 'package:cv_desing_website_flutter/presentation/shared/widgets/custom_form_field.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ComplementaryFormationsForm extends StatelessWidget {
@@ -28,73 +26,7 @@ class ComplementaryFormationsForm extends StatelessWidget {
         );
       },
     );
-
-    return Form(
-      child: Column(
-        children: [
-          ElevatedButton.icon(
-            onPressed: () {},
-            icon: const Icon(Icons.add),
-            label: const Text('Añadir'),
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              prefixIcon: Icon(Icons.science_outlined),
-              labelText: 'Curso / taller / seminario / conferencia',
-            ),
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              prefixIcon: Icon(Icons.school_outlined),
-              labelText: 'Entidad formadora',
-            ),
-          ),
-          TextFormField(
-            keyboardType: TextInputType.number,
-            inputFormatters: [
-              FilteringTextInputFormatter.digitsOnly,
-            ],
-            decoration: const InputDecoration(
-              prefixIcon: Icon(Icons.timer_outlined),
-              labelText: 'Horas de estudio',
-            ),
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: _datePicker(context, text: 'Desde'),
-              ),
-              const SizedBox(
-                width: 8,
-              ),
-              Expanded(
-                child: _datePicker(context, text: 'Hasta'),
-              ),
-            ],
-          )
-        ],
-      ),
-    );
   }
-
-  Widget _datePicker(BuildContext context, {required String text}) =>
-      TextFormField(
-        enableInteractiveSelection: false,
-        keyboardType: TextInputType.datetime,
-        decoration: InputDecoration(
-          prefixIcon: const Icon(Icons.date_range_rounded),
-          labelText: text,
-        ),
-        onTap: () => _selectDate(context),
-      );
-
-  Future<DateTime?> _selectDate(BuildContext context) => showDatePicker(
-        // locale: const Locale('es', 'ES'),
-        context: context,
-        initialDate: DateTime.now(),
-        firstDate: DateTime(1850),
-        lastDate: DateTime.now(),
-      );
 
   Widget _buildComplementaryTraining({
     ComplementaryTraining? editedComplementaryTraining,
