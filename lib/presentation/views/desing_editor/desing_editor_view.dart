519bad2e4258b90039f3f6f2c15057e771c9bb83
xenxi <antoniom.diaz.moreno@gmail.com>
2022-05-28 17:16:01 +0200

    :recycle: refact extract method

--- lib/presentation/views/desing_editor/desing_editor_view.dart
+++ lib/presentation/views/desing_editor/desing_editor_view.dart
@@ -3,6 +3,7 @@ import 'package:cv_desing_website_flutter/application/editor/sections.dart';
 import 'package:cv_desing_website_flutter/presentation/core/dependency_injections/ioc.dart';
 import 'package:cv_desing_website_flutter/presentation/shared/components/adaptative_funtions.dart';
 import 'package:cv_desing_website_flutter/presentation/views/desing_editor/widgets/form_step_builder.dart';
+import 'package:cv_desing_website_flutter/presentation/views/desing_editor/widgets/resume_form.dart';
 import 'package:cv_desing_website_flutter/presentation/views/desing_editor/widgets/resume_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -51,33 +52,9 @@ class DesingEditorView extends HookWidget {
     );
   }
 
-  BlocBuilder<CvEditorBloc, CvEditorState> _buildForm() {
-    return BlocBuilder<CvEditorBloc, CvEditorState>(
-      builder: (context, state) {
-        return Expanded(
-          child: Stepper(
-            controlsBuilder: (context, details) {
-              return Container();
-            },
-            currentStep: _getCurrentStepFrom(state),
-            onStepTapped: (index) => _updateSelectedSection(context, index),
-            steps: _buildStepForms(),
-          ),
-        );
-      },
+  Widget _buildForm() {
+    return const Expanded(
+      child: ResumeForm(),
     );
   }
-
-  void _updateSelectedSection(BuildContext context, int index) =>
-      BlocProvider.of<CvEditorBloc>(context)
-          .add(SectionChanged(section: Section.values[index]));
-
-  int _getCurrentStepFrom(CvEditorState state) =>
-      Section.values.indexOf(state.section);
-
-  List<Step> _buildStepForms() => Section.values
-      .map(
-        (section) => FormStepBuilder.fromSection(section),
-      )
-      .toList();
 }
---
+++ lib/presentation/views/desing_editor/widgets/resume_form.dart
@@ -0,0 +1,36 @@
+import 'package:cv_desing_website_flutter/application/editor/cv_editor_bloc.dart';
+import 'package:cv_desing_website_flutter/application/editor/sections.dart';
+import 'package:cv_desing_website_flutter/presentation/views/desing_editor/widgets/form_step_builder.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ResumeForm extends StatelessWidget {
+  const ResumeForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<CvEditorBloc, CvEditorState>(
+      builder: (context, state) {
+        return Stepper(
+          controlsBuilder: (context, details) {
+            return Container();
+          },
+          currentStep: _getCurrentStepFrom(state),
+          onStepTapped: (index) => _updateSelectedSection(context, index),
+          steps: [..._buildStepForms()],
+        );
+      },
+    );
+  }
+
+  void _updateSelectedSection(BuildContext context, int index) =>
+      BlocProvider.of<CvEditorBloc>(context)
+          .add(SectionChanged(section: Section.values[index]));
+
+  int _getCurrentStepFrom(CvEditorState state) =>
+      Section.values.indexOf(state.section);
+
+  Iterable<Step> _buildStepForms() => Section.values.map(
+        (section) => FormStepBuilder.fromSection(section),
+      );
+}
