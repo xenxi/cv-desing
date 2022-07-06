9079e791b135f340d212929171f1c4bc57a365e0
xenxi <antoniom.diaz.moreno@gmail.com>
2022-07-06 06:35:06 +0200

    :lipstick: :construction: create new stepper

--- lib/presentation/views/desing_editor/widgets/resume_form.dart
+++ lib/presentation/views/desing_editor/widgets/resume_form.dart
@@ -1,5 +1,6 @@
 import 'package:cv_desing_website_flutter/application/editor/cv_editor_bloc.dart';
 import 'package:cv_desing_website_flutter/application/editor/sections.dart';
+import 'package:cv_desing_website_flutter/presentation/shared/logo.dart';
 import 'package:cv_desing_website_flutter/presentation/views/desing_editor/widgets/form_step_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -11,6 +12,57 @@ class ResumeForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<CvEditorBloc, CvEditorState>(
       builder: (context, state) {
+        return Row(
+          children: [
+            Container(
+              color: Colors.red,
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+              width: 200,
+              child: Column(
+                children: [
+                  const Logo(height: 120),
+                  const SizedBox(
+                    height: 22,
+                  ),
+                  ...Section.values.map(
+                    (e) => Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.blue,
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                              child: Icon(Icons.telegram),
+                            ),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            Expanded(
+                              child: Text('$e'),
+                            )
+                          ],
+                        ),
+                        if (Section.values.indexOf(e) <
+                            Section.values.length - 1)
+                          Container(
+                            margin: const EdgeInsets.only(left: 4.5),
+                            height: 30,
+                            child: const VerticalDivider(
+                              thickness: 3,
+                              color: Colors.black,
+                            ),
+                          )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        );
         return Stepper(
           controlsBuilder: (context, details) {
             return Container();
