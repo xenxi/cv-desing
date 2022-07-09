9fab03a1b4640725914f34cfceed9f143aa5dcf4
xenxi <antoniom.diaz.moreno@gmail.com>
2022-07-09 10:00:46 +0200

    :fire:

--- lib/presentation/views/desing_editor/widgets/form_step_builder.dart
+++ lib/presentation/views/desing_editor/widgets/form_step_builder.dart
@@ -1,6 +1,5 @@
 import 'package:cv_desing_website_flutter/application/editor/sections.dart';
 import 'package:cv_desing_website_flutter/presentation/shared/components/section_extensions.dart';
-import 'package:cv_desing_website_flutter/presentation/shared/values/location.dart';
 import 'package:cv_desing_website_flutter/presentation/views/desing_editor/widgets/academic_training_form.dart';
 import 'package:cv_desing_website_flutter/presentation/views/desing_editor/widgets/complementary_formations_form.dart';
 import 'package:cv_desing_website_flutter/presentation/views/desing_editor/widgets/contact_info_form.dart';
@@ -20,29 +19,6 @@ class FormStepBuilder extends Step {
       title: Text(section.displayName),
       content: _buildContent(section),
     );
-  }
-
-  static String _buildTitle(Section section) {
-    switch (section) {
-      case Section.personalInformation:
-        return Location.personalInformation;
-      case Section.contactInformation:
-        return Location.conactInformation;
-      case Section.academicTraining:
-        return Location.academicTraining;
-      case Section.complementaryFormations:
-        return Location.complementaryFormations;
-      case Section.workExperience:
-        return Location.workExperience;
-      case Section.languages:
-        return Location.languages;
-      case Section.softwareSkills:
-        return Location.softwareSkills;
-      case Section.skillsandAptitudes:
-        return Location.skillsandAptitudes;
-      default:
-        return '';
-    }
   }
 
   static Widget _buildContent(Section section) {
