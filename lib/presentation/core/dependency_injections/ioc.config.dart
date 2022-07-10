0f8e52f3e0d0d6b2b1e114d3320c271d52595209
xenxi <antoniom.diaz.moreno@gmail.com>
2022-07-10 18:39:00 +0200

    :hammer:

--- lib/presentation/core/dependency_injections/ioc.config.dart
+++ lib/presentation/core/dependency_injections/ioc.config.dart
@@ -4,35 +4,37 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
-import 'package:firebase_auth/firebase_auth.dart' as _i6;
-import 'package:flutter/material.dart' as _i7;
+import 'package:firebase_auth/firebase_auth.dart' as _i7;
+import 'package:flutter/material.dart' as _i8;
 import 'package:get_it/get_it.dart' as _i1;
-import 'package:google_sign_in/google_sign_in.dart' as _i8;
+import 'package:google_sign_in/google_sign_in.dart' as _i9;
 import 'package:injectable/injectable.dart' as _i2;
 
-import '../../../application/auth/auth_bloc.dart' as _i19;
-import '../../../application/auth/sign_in_form/sign_in_form_bloc.dart' as _i18;
-import '../../../application/components/image_picker.dart' as _i13;
-import '../../../application/desings/desings_bloc.dart' as _i23;
-import '../../../application/desings/details/desing_details_bloc.dart' as _i22;
+import '../../../application/auth/auth_bloc.dart' as _i20;
+import '../../../application/auth/sign_in_form/sign_in_form_bloc.dart' as _i19;
+import '../../../application/components/image_picker.dart' as _i14;
+import '../../../application/desings/desings_bloc.dart' as _i24;
+import '../../../application/desings/details/desing_details_bloc.dart' as _i23;
 import '../../../application/editor/academy_training_form/academy_training_form_bloc.dart'
     as _i3;
 import '../../../application/editor/complementary_training_form/complementary_training_form_bloc.dart'
     as _i4;
+import '../../../application/editor/contact_information_form/contact_information_form_bloc.dart'
+    as _i6;
 import '../../../application/editor/cv_editor_bloc.dart' as _i5;
 import '../../../application/editor/personal_information_form/personal_information_form_bloc.dart'
-    as _i17;
-import '../../../application/navigation/navigation_bloc.dart' as _i16;
-import '../../../domain/auth/i_auth_facade.dart' as _i9;
-import '../../../domain/desing.dart' as _i15;
-import '../../../domain/i_desings.dart' as _i20;
-import '../../../infrastructure/auth/firebase_auth_facade.dart' as _i10;
-import '../../../infrastructure/core/register_module.dart' as _i24;
-import '../../../infrastructure/file_picker_image_picker.dart' as _i14;
-import '../../../infrastructure/in_memory_desings.dart' as _i21;
-import '../routes/navigators/custom_navigator.dart' as _i12;
+    as _i18;
+import '../../../application/navigation/navigation_bloc.dart' as _i17;
+import '../../../domain/auth/i_auth_facade.dart' as _i10;
+import '../../../domain/desing.dart' as _i16;
+import '../../../domain/i_desings.dart' as _i21;
+import '../../../infrastructure/auth/firebase_auth_facade.dart' as _i11;
+import '../../../infrastructure/core/register_module.dart' as _i25;
+import '../../../infrastructure/file_picker_image_picker.dart' as _i15;
+import '../../../infrastructure/in_memory_desings.dart' as _i22;
+import '../routes/navigators/custom_navigator.dart' as _i13;
 import '../routes/navigators/i_navigator.dart'
-    as _i11; // ignore_for_file: unnecessary_lambdas
+    as _i12; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -43,29 +45,30 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
   gh.factory<_i3.AcademyTrainingFormBloc>(() => _i3.AcademyTrainingFormBloc());
   gh.factory<_i4.ComplementaryTrainingFormBloc>(
       () => _i4.ComplementaryTrainingFormBloc());
-  gh.factory<_i5.CvEditorBloc>(() => _i5.CvEditorBloc());
-  gh.lazySingleton<_i6.FirebaseAuth>(() => registerModule.firebaseAuth);
-  gh.lazySingleton<_i7.GlobalKey<_i7.NavigatorState>>(() => registerModule.key);
-  gh.lazySingleton<_i8.GoogleSignIn>(() => registerModule.googleSignIn);
-  gh.lazySingleton<_i9.IAuthFacade>(() => _i10.FirebaseAuthFacade(
-      get<_i6.FirebaseAuth>(), get<_i8.GoogleSignIn>()));
-  gh.factory<_i11.INavigator>(
-      () => _i12.CustomNavigator(get<_i7.GlobalKey<_i7.NavigatorState>>()));
-  gh.factory<_i13.ImagePicker>(() => _i14.FilePickerImagePicker());
-  gh.lazySingleton<List<_i15.Desing>>(() => registerModule.desings);
-  gh.factory<_i16.NavigationBloc>(
-      () => _i16.NavigationBloc(get<_i11.INavigator>()));
-  gh.factory<_i17.PersonalInformationFormBloc>(
-      () => _i17.PersonalInformationFormBloc(get<_i13.ImagePicker>()));
-  gh.factory<_i18.SignInFormBloc>(
-      () => _i18.SignInFormBloc(get<_i9.IAuthFacade>()));
-  gh.factory<_i19.AuthBloc>(() => _i19.AuthBloc(get<_i9.IAuthFacade>()));
-  gh.lazySingleton<_i20.IDesings>(
-      () => _i21.InMemoryDesings(get<List<_i15.Desing>>()));
-  gh.factory<_i22.DesingDetailsBloc>(
-      () => _i22.DesingDetailsBloc(get<_i20.IDesings>()));
-  gh.factory<_i23.DesingsBloc>(() => _i23.DesingsBloc(get<_i20.IDesings>()));
+  gh.factory<_i5.CvEditorBloc>(
+      () => _i5.CvEditorBloc(get<_i6.ContactInformationFormBloc>()));
+  gh.lazySingleton<_i7.FirebaseAuth>(() => registerModule.firebaseAuth);
+  gh.lazySingleton<_i8.GlobalKey<_i8.NavigatorState>>(() => registerModule.key);
+  gh.lazySingleton<_i9.GoogleSignIn>(() => registerModule.googleSignIn);
+  gh.lazySingleton<_i10.IAuthFacade>(() => _i11.FirebaseAuthFacade(
+      get<_i7.FirebaseAuth>(), get<_i9.GoogleSignIn>()));
+  gh.factory<_i12.INavigator>(
+      () => _i13.CustomNavigator(get<_i8.GlobalKey<_i8.NavigatorState>>()));
+  gh.factory<_i14.ImagePicker>(() => _i15.FilePickerImagePicker());
+  gh.lazySingleton<List<_i16.Desing>>(() => registerModule.desings);
+  gh.factory<_i17.NavigationBloc>(
+      () => _i17.NavigationBloc(get<_i12.INavigator>()));
+  gh.factory<_i18.PersonalInformationFormBloc>(
+      () => _i18.PersonalInformationFormBloc(get<_i14.ImagePicker>()));
+  gh.factory<_i19.SignInFormBloc>(
+      () => _i19.SignInFormBloc(get<_i10.IAuthFacade>()));
+  gh.factory<_i20.AuthBloc>(() => _i20.AuthBloc(get<_i10.IAuthFacade>()));
+  gh.lazySingleton<_i21.IDesings>(
+      () => _i22.InMemoryDesings(get<List<_i16.Desing>>()));
+  gh.factory<_i23.DesingDetailsBloc>(
+      () => _i23.DesingDetailsBloc(get<_i21.IDesings>()));
+  gh.factory<_i24.DesingsBloc>(() => _i24.DesingsBloc(get<_i21.IDesings>()));
   return get;
 }
 
-class _$RegisterModule extends _i24.RegisterModule {}
+class _$RegisterModule extends _i25.RegisterModule {}
