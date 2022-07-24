3b4608f22319eb5038686c5473da5bbb6521f2f7
xenxi <antoniom.diaz.moreno@gmail.com>
2022-07-24 06:49:05 +0200

    :lipstick: update template button

--- lib/presentation/views/desing_editor/widgets/cv_template_button.dart
+++ lib/presentation/views/desing_editor/widgets/cv_template_button.dart
@@ -1,5 +1,6 @@
 import 'package:cv_desing_website_flutter/application/editor/cv_editor_actor/cv_editor_actor_bloc.dart';
 import 'package:cv_desing_website_flutter/application/editor/cv_editor_actor/cv_templates.dart';
+import 'package:cv_desing_website_flutter/presentation/core/custom_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -14,24 +15,37 @@ class CvTemplateButton extends StatelessWidget {
       buildWhen: (previous, current) => previous.template != current.template,
       builder: (context, state) {
         return Card(
+          color: CustomTheme.primaryColor,
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            padding: const EdgeInsets.symmetric(horizontal: 8),
             child: Row(
               mainAxisSize: MainAxisSize.min,
               children: [
+                const Icon(
+                  Icons.design_services_outlined,
+                  color: Colors.white,
+                ),
+                const SizedBox(width: 8),
                 ...CvTemplates.values.map(
                   (e) => Row(
                     children: [
                       Row(
                         children: [
                           Radio<CvTemplates>(
+                            fillColor: MaterialStateColor.resolveWith(
+                              (states) => Colors.white,
+                            ),
+                            splashRadius: 18,
                             value: e,
                             groupValue: state.template,
                             onChanged: (_) =>
                                 BlocProvider.of<CvEditorActorBloc>(context)
                                     .add(TemplateChanged(e)),
                           ),
-                          Text(e.name)
+                          Text(
+                            e.name,
+                            style: const TextStyle(color: Colors.white),
+                          )
                         ],
                       ),
                     ],
